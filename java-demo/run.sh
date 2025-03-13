2c8434c92a268f212f258080f8ed1e0a52537cb1
Remco Beckers <remco.beckers@suse.com>
2025-03-13 10:59:10 +0100

    More config for local collector

--- java-demo/run.sh
+++ java-demo/run.sh
@@ -6,7 +6,4 @@ if ! [ -f "lib/opentelemetry-javaagent.jar" ]; then
   curl -L -o lib/opentelemetry-javaagent.jar https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
 fi
 
-export JAVA_TOOL_OPTIONS="-javaagent:lib/opentelemetry-javaagent.jar"
-export OTEL_SERVICE_NAME="dice-demo"
-export OTEL_RESOURCE_ATTRIBUTES="service.namespace=dice-demo"
-java -jar ./build/libs/java-demo.jar
+JAVA_TOOL_OPTIONS="-javaagent:lib/opentelemetry-javaagent.jar" OTEL_SERVICE_NAME="dice-demo" OTEL_RESOURCE_ATTRIBUTES="service.namespace=dice-demo" java -jar ./build/libs/java-demo.jar
