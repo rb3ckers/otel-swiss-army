4b5ced390df1e94d7adf3445104f4bc2409fb994
Remco Beckers <remco.beckers@suse.com>
2025-03-13 09:25:55 +0100

    Add example for counter

--- java-demo/RollController.java
+++ java-demo/RollController.java
@@ -7,27 +7,40 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 
 @RestController
 public class RollController {
   private static final Logger logger = LoggerFactory.getLogger(RollController.class);
   private final Tracer tracer;
+  private final Meter meter;
+  private final LongCounter rollCounter;
 
   public RollController() {
     tracer = GlobalOpenTelemetry.getTracer(this.getClass().getName());
+    meter = GlobalOpenTelemetry.getMeter(this.getClass().getName());
+    rollCounter =
+        meter
+            .counterBuilder("java.demo.roll.counter")
+            .setDescription("A count of dice roles")
+            .build();
   }
 
   @GetMapping("/rolldice")
   public String index(@RequestParam("player") Optional<String> player) {
     var span = tracer.spanBuilder("roll-the-dice").startSpan();
-
+    
     int result = this.getRandomNumber(1, 6);
     if (player.isPresent()) {
       logger.info("{} is rolling the dice: {}", player.get(), result);
+      rollCounter.add(1, Attributes.of("player", player.get()));
     } else {
       logger.info("Anonymous player is rolling the dice: {}", result);
+      rollCounter.add(1);
     }
     
     span.setAttribute("result", result);
